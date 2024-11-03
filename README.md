7c152943c7cf91370ac0887d15a762ed09fd7b7d
waffiq <waffiqmaaroja1998@gmail.com>
2024-11-03 16:40:17 +0700

    add instructions

---
+++ README.md
@@ -0,0 +1,16 @@
+## Text to Sign Language
+### Installation
+Activate virtual environment
+```
+py -m venv .venv
+.venv/Scripts/activate
+```
+Install dependencies
+```
+pip install -r requirements.txt
+```
+
+### Run
+```
+streamlit run app.py
+```
--- utils/video.py
+++ utils/video.py
@@ -11,20 +11,22 @@ def get_info():
     return fn, str(ct)
 
 
-__history_path = './results/history.txt'
-
-
 def generate_video(paths: list[str]):
     fn, t = get_info()
     clips = [VideoFileClip(path) for path in paths]
     final_clip = concatenate_videoclips(clips, method='compose')
-    path = os.path.join('./results/generated', fn)
+    dir = './results/generated'
+    os.makedirs(dir, exist_ok=True)
+    path = os.path.join(dir, fn)
     final_clip.write_videofile(path)
     return path, t
 
 
+__history_path = './results/history.txt'
+
+
 def append_info(t, fn, text):
-    with open(__history_path, 'a') as f:
+    with open(__history_path, 'a+') as f:
         f.write(t + '\n')
         f.write(fn + '\n')
         f.write(text + '\n')
